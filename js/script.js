6d1245119171dd3a8ee97d37b116c7d8b3ae671a
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-06-16 14:44:30 +0200

    Esercizio finito

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-
+            email: '',
+            arrayEmail: []
         }
     },
     mounted(){
-        
+        this.createEmail();
     },
     methods:{
-        
+        createEmail(){
+            for(let i = 1; i <= 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) => {
+                    this.arrayEmail.push(email.data.response);
+                });
+            }
+        }
     },
 }).mount('#app')
